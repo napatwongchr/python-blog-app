175ee7b418ed5b59926b6227bf285e38a3f866e7
knotnapat <napat_won@true-e-logistics.com>
2021-03-12 10:52:59 +0700

    feat: create post

--- backend/myproject/posts/views.py
+++ backend/myproject/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 posts = {
     "data": [
@@ -11,10 +13,19 @@ posts = {
     ]
 }
 
+@csrf_exempt
 def post_list(request):
-  response = JsonResponse(data=posts)
-  response.status_code = 200
-  return response
+  if (request.method == "GET"):
+    response = JsonResponse(data=posts)
+    response.status_code = 200
+    return response
+
+  if (request.method == "POST"):
+    data = json.loads(request.body)
+    posts["data"].append({ "id": "2", "title": data["title"], "content": data["content"] })
+    response = JsonResponse(data={ "message": "created post successfully." })
+    response.status_code = 201
+    return response
 
 def single_post_detail(request, post_id):
   response = JsonResponse({ "data": {} })

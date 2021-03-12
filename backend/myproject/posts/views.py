a368c2e9bcf0c00d27d0548711030bb42eb644f9
knotnapat <napat_won@true-e-logistics.com>
2021-03-12 08:49:20 +0700

    feat: return mocked post list in json

--- backend/myproject/posts/views.py
+++ backend/myproject/posts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
+posts = {
+    "data": [
+        {
+            "id": 1,
+            "title": "Post #1",
+            "content": "This is post #1 content"
+        }
+    ]
+}
+
 def post_list(request):
-    return HttpResponse("Hello, this is posts app")
+    response = JsonResponse(posts)
+    response.status_code = 200
+    return response

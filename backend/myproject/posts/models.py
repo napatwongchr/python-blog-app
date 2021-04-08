c13ffc6d85057a67ef8488360fc0eff4fc277695
knotnapat <napat_won@true-e-logistics.com>
2021-04-08 15:21:09 +0700

    add comment model

--- backend/myproject/posts/models.py
+++ backend/myproject/posts/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class Post(models.Model):
   title = models.CharField(max_length=150)
   content = models.CharField(max_length=2000)
+
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  comment = models.TextField()
+  created_on = models.DateTimeField()
+  updated_on = models.DateTimeField(auto_now=True)
+
+  class Meta:
+   db_table = "comments"

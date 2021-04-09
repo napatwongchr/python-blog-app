1acee1d7c75e8ab879c55b91bfec51be7db53f53
knotnapat <napat_won@true-e-logistics.com>
2021-04-09 09:44:29 +0700

    add signup api

---
+++ backend/myproject/authentication/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", "")
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- backend/myproject/authentication/views.py
+++ backend/myproject/authentication/views.py
@@ -1,6 +1,12 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import UserSerializer
 
 @api_view(['POST'])
 def signup(request):
-  return Response({ "status": "ok" })
+  serializer = UserSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response({ "message": "register successfully" }, status=status.HTTP_201_CREATED)
+  return Response({ "message": "register failed", "errors": serializer.errors }, status=status.HTTP_400_BAD_REQUEST)
